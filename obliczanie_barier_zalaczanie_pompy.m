8771cb7cd2cae52f2d85abe350d852191291b8e3
rafalbrodziak <84399925+rafalbrodziak@users.noreply.github.com>
2021-05-23 20:36:41 +0200

    Update obliczanie_barier_zalaczanie_pompy.m

--- obliczanie_barier_zalaczanie_pompy.m
+++ obliczanie_barier_zalaczanie_pompy.m
@@ -2,5 +2,5 @@
 
 disp('zalaczanie pompy:')
 disp(pompa_do_zalaczenia);
-p_2S{bariera}(pompa_do_zalaczenia,1) = 1; % zmien macierz pomp do za³¹czenia o wybran¹ pompê w barierze
+p_2S{bariera}(pompa_do_zalaczenia,1) = 1; % change the matrix of pumps to be switched on with the selected pump in the barrier 
 run obliczanie_wydajnosci_bariery_obliczonej.m
